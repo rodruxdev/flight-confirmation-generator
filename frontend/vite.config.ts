634e5649754fb15f7fdd5b50de0ceb14c1e580ea
Rodrigo Goitia <rodrigojgq@gmail.com>
2026-01-24 17:43:36 -0300

    chore: add config to deploy project to github page (#7)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GH_PAGES_BASE ?? '/flight-confirmation-generator/',
   plugins: [react()],
 })
